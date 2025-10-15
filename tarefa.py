c4f87c0e02da55fe36adc884462a7f22d1e17af0
Maurício <devjuniormauricio@gmail.com>
2025-10-14 22:13:52 -0300

    Retornando uma tarefa

--- tarefa.py
+++ tarefa.py
@@ -16,3 +16,12 @@ def buscar_tarefas():
         }
     ]
     return jsonify(tarefas)
+
+def buscar_tarefa():
+    tarefa = {
+        'id': 1,
+        'nome': 'Aprender digitação',
+        'descricao': 'Vamos aumentar o zoom para não errar a digitação',
+        'status': 'Pendente'
+    }
+    return jsonify(tarefa)
